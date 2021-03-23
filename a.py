f76ed13eadfd8b3e02cd18d93c5e5b79036d0622
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2021-03-23 13:47:01 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,22 @@
+import pandas
+# If you don't have pandas in your computer write "-pip install pandas" in your terminal!
+
+# Write your excel file's path before you run the code!!!!!!
+path = str(input("Write your excel file's path: "))
+path = path.replace("\\", "/")
+filename = input("Write excel file name without .xlsx: ")
+
+if path[-1] == "/":
+    pass
+else:
+    path = path+"/"
+
+data = pandas.read_excel(path+filename+".xlsx")
+columnname = input("Write columnn name: ")
+columndata = data[columnname].unique()
+
+# Creates an excel file with the name of that value for each different value in a column.
+for i in columndata:
+
+    a = data[data[columnname].str.contains(i)]
+    a.to_excel(path+i+".xlsx")
